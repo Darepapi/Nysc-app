19aa36b65af0e29da51b23b54b2de30e20e74439
Dami <32749185+Darepapi@users.noreply.github.com>
2020-01-03 13:30:33 +0100

    Minimal NYSC Verification File
    A little  python lines of code using the basics (if, elif, while etc) to verify if a candidate 
    is eligible for the NYSC program and redirects to The Exemption Certification page if the
    Candidate is above the required maximum age

---
+++ NYSC APP.py
@@ -0,0 +1,29 @@
+print("Hello ! \n Welcome to the Official page of NYSC.")
+
+x=int(input("If you are here for Mobilization, Please press 1 or enter  2 to apply for Exemption Certificate"))
+while x!=1 and x!=2:
+    print("You just entered an incorrect value! ")
+    x=int(input("Press 1 for NYSC or 2 for Exemption Certificate.."))
+if x==1:
+    print("N.Y.S.C MOBILIZATION")
+    names=input("Enter your username: ")
+    print("Welcome, User", names)
+    a=int(input("Please enter your Year  of birth: "))
+    resultedYear= 2019-a
+    if resultedYear>30:
+        print("Hello, Dear",names,", Your age is above 30 and you can't be mobilized for N.Y.S.C. \nPlease, apply for Exemption Form")
+    else:
+        print("Congratulation! Dear ",names, " You are eligible for Mobilization. \n An application form will be sent to the provided email.")
+elif x==2:
+    print("EXEMPTION CERTIFICATION")
+    nameE=input("Please enter your Username:")
+    print("Welcome, User", nameE)
+    print("Please fill in the follow details for the application of Exemption Certificate..")
+    dateOfBirth=int(input("Date Of Birth: "))
+    month=input("Month Of Birth: ")
+    year=int(input("Year Of Birth: "))
+    yearCalculated=2019-year
+    print("Congratulation, Dear User ",nameE,"!  \n Your currently calculated age is  ",yearCalculated,". An application form will be sent to the provided email.")
+    
+
+
